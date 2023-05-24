96401e6915217190cea2206e34365bf971543d88
himanshp1656 <126962282+himanshp1656@users.noreply.github.com>
2023-05-24 11:25:18 -0700

    Update settings.py

--- avan/settings.py
+++ avan/settings.py
@@ -132,4 +132,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-OPENAI_KEY='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'
+OPENAI_KEY='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'
